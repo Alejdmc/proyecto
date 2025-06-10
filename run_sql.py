9dbf3228f54b215e013f28a310cc2e231c379ef8
Alejandro Monroy <damonroy86ucatolica.edu.co>
2025-06-09 18:00:31 -0600

    99.1.1%

--- run_sql.py
+++ run_sql.py
@@ -12,10 +12,14 @@ engine = create_async_engine(DATABASE_URL, echo=True)
 
 async def run_alter():
     async with engine.begin() as conn:
-        await conn.execute(text("ALTER TABLE artistadb ADD COLUMN eliminado BOOLEAN DEFAULT FALSE"))
-        await conn.execute(text("ALTER TABLE artistadb ADD COLUMN imagen_url VARCHAR"))
-        await conn.execute(text("ALTER TABLE canciondb ADD COLUMN eliminado BOOLEAN DEFAULT FALSE"))
-        await conn.execute(text("ALTER TABLE canciondb ADD COLUMN imagen_url VARCHAR"))
+        # Artista
+        await conn.execute(text("ALTER TABLE artistadb ADD COLUMN IF NOT EXISTS eliminado BOOLEAN DEFAULT FALSE"))
+        await conn.execute(text("ALTER TABLE artistadb ADD COLUMN IF NOT EXISTS imagen_url VARCHAR"))
+        await conn.execute(text("ALTER TABLE artistadb ADD COLUMN IF NOT EXISTS imagen_bytes BYTEA"))
+        # Canción
+        await conn.execute(text("ALTER TABLE canciondb ADD COLUMN IF NOT EXISTS eliminado BOOLEAN DEFAULT FALSE"))
+        await conn.execute(text("ALTER TABLE canciondb ADD COLUMN IF NOT EXISTS imagen_url VARCHAR"))
+        await conn.execute(text("ALTER TABLE canciondb ADD COLUMN IF NOT EXISTS imagen_bytes BYTEA"))
     await engine.dispose()
 
 if __name__ == "__main__":
